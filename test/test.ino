fc73128538c89b8513e80868a80031dd30b6c301
Eric <ericl16384@gmail.com>
2023-03-30 06:44:48 -0700

    working leg controller in test.ino

--- test/test.ino
+++ test/test.ino
@@ -25,31 +25,46 @@ Servo servoShoulder;
 Servo servoKnee;
 
 float legLength = 100; // sum of both segments
-float x = 200;
-float y = -100;
+float x = -60;
+float y = -60;
 
 float distance;
 float angle1;
 float angle2;
 
 void updateServos() {
-  servoShoulder.write(angle1 + 180);
+  servoShoulder.write(angle1);
   servoKnee.write(angle2); // todo
 }
 
-void updateAngles() {
+bool updateAngles() {
   distance = sqrt(pow(x, 2) + pow(y, 2));
+
+  angle1 = 180;
+  angle2 = 0;
+
+  // distance
+  if(distance / legLength < 1) {
+    angle1 += toDegrees(asin(distance / legLength)) * -1;
+    angle2 += toDegrees(asin(distance / legLength)) * 2;
+  } else if(distance / legLength == 1) {
+    angle1 += -90;
+    angle2 += 180;
+  } else {
+    return false;
+  }
+
+//  Serial.println(angle1);
+
+  // angle
+  if(distance > 0) {
+    angle1 += toDegrees(atan2(y, x)) + 90;
+  }
+  Serial.println(toDegrees(atan2(y, x)) + 90);
   
-  angle1 = toDegrees(atan2(y, x));
-  angle2 = acos(distance / legLength);
-
-//  angle1 -= ang
-
-
-
-  // debug
-  angle2 = 180;
+  return true;
 }
+
 
 
 void setup() {
@@ -62,21 +77,50 @@ void setup() {
 
 
   servoShoulder.write(0);
+  servoKnee.write(180);
+  delay(1000);
+  
+  servoShoulder.write(180);
   servoKnee.write(0);
   delay(1000);
+
+//   DEBUG
+//  delay(1000000);
+
+
+
+//  Serial.println(toDegrees(atan2(0, 0)));
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  x -= 1;
+  x += 0.2;
+//  y -= 0.5;
 
-  updateAngles();
-  updateServos();
+  if(updateAngles()) { updateServos(); }
 
 //  Serial.println(angle1);
 //  Serial.println(distance);
-  Serial.println(acos(distance / legLength));
+//  Serial.println(angle2);
+
+//  Serial.print("x:");
+//  Serial.print(x);
+//  Serial.print(",");
+//
+//  Serial.print("y:");
+//  Serial.print(y);
+//  Serial.print(",");
+//
+//  Serial.print("a1:");
+//  Serial.print(angle1);
+//  Serial.print(",");
+//
+//  Serial.print("a2:");
+//  Serial.print(angle2);
+//  Serial.print(",");
+//
+//  Serial.println("");
 
   delay(10);
 }
